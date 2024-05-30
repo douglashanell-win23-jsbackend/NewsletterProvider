778dcca0bade3c715abc0e19f8dc291184a677cd
Douglas3478 <Douglas_00@live.se>
2024-05-30 14:02:42 +0200

    Add project files.

---
+++ NewsletterProvider/Functions/Unsubscribe.cs
@@ -0,0 +1,39 @@
+using Data.Contexts;
+using Data.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+
+namespace NewsletterProvider.Functions
+{
+    public class Unsubscribe(ILogger<Unsubscribe> logger, DataContext context)
+    {
+        private readonly ILogger<Unsubscribe> _logger = logger;
+        private readonly DataContext _context = context;
+
+        [Function("Unsubscribe")]
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
+        {
+            var body = await new StreamReader(req.Body).ReadToEndAsync();
+            if (!string.IsNullOrEmpty(body))
+            {
+                var subscribeEntity = JsonConvert.DeserializeObject<SubscribeEntity>(body);
+                if (subscribeEntity != null)
+                {
+                    var existingSubscriber = await _context.Subscribers.FirstOrDefaultAsync(s => s.Email == subscribeEntity.Email);
+                    if (existingSubscriber != null)
+                    {
+                        _context.Remove(existingSubscriber);
+                        await _context.SaveChangesAsync();
+                        return new OkObjectResult(new { Status = 200, Message ="You're now unsubscribed." });
+                    }
+                }
+            }
+            return new BadRequestObjectResult(new { Status = 400, Message = "Unable to unsubscribe, try again later." });
+        }
+    }
+}
